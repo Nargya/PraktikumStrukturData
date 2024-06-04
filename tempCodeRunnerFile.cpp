226c46de7ccd64340263f1f62f44f1f5e6861720
Nargya <naufalalifcilacap@gmail.com>
2024-06-04 08:26:13 +0700

    first commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr [i] << {[2],[5],[5],[6],[8],[10],[11]};
+    {
+        int low = {[2, i >= 2, i++]};
+        int high = {[11, i <= 11, i++]};
+    }
+    
+    for ([2 < i < 11]) << find[i];
+    cin << for ([2 < i < 11]) << find[i] << "Input : ";
+    cout << "\nData ditemukan di indeks ke-N" << endl;{
+        return 0;
+    } 
+
+}
